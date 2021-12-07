236d4a715edfaf3b475d6e462ce1d65f4454fa42
Pablo Lucero <pslp.gen@gmail.com>
2021-12-07 18:31:25 -0300

    Fix parse_fe_comp_consultar_response

--- lib/snoopy_afip/authorize_adapter.rb
+++ lib/snoopy_afip/authorize_adapter.rb
@@ -162,10 +162,10 @@ module Snoopy
       result_get               = fe_comp_consultar_result[:result_get]
 
       unless result_get.nil?
-        # bill.result            = result_get[:resultado]
-        # bill.number            = result_get[:cbte_desde]
-        # bill.cae               = result_get[:cod_autorizacion]
-        # bill.due_date_cae      = result_get[:fch_vto]
+        bill.result              = result_get[:resultado]
+        bill.number              = result_get[:cbte_desde]
+        bill.cae                 = result_get[:cod_autorizacion]
+        bill.due_date_cae        = result_get[:fch_vto]
         # bill.imp_iva           = result_get[:imp_iva]
         # bill.document_num      = result_get[:doc_numero]
         # bill.process_date      = result_get[:fch_proceso]
